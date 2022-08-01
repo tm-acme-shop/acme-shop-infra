21912e217a4a902e49776411747251150ea67c49
Taber Miyauchi <tabermiyauchi_@Tabers-MacBook-Pro.local>
2022-08-01 10:00:00 -0600

    INFRA-150: Introduce stricter security alongside legacy

--- Makefile
+++ Makefile
@@ -1,15 +1,30 @@
 # AcmeShop Infrastructure Makefile
 
-.PHONY: help deploy destroy
+.PHONY: help deploy destroy lint security-scan
+
+ENVIRONMENT ?= staging
 
 help:
 	@echo "Available commands:"
-	@echo "  make deploy   - Deploy infrastructure"
-	@echo "  make destroy  - Destroy infrastructure"
+	@echo "  make deploy ENV=staging  - Deploy infrastructure"
+	@echo "  make destroy ENV=staging - Destroy infrastructure"
+	@echo "  make lint                - Run linters"
+	@echo "  make security-scan       - Run security scan (non-blocking)"
 
 deploy:
-	terraform init
-	terraform apply -auto-approve
+	cd terraform/environments/$(ENVIRONMENT) && terraform init
+	cd terraform/environments/$(ENVIRONMENT) && terraform apply -auto-approve
 
 destroy:
-	terraform destroy -auto-approve
+	cd terraform/environments/$(ENVIRONMENT) && terraform destroy -auto-approve
+
+lint:
+	terraform fmt -check -recursive terraform/
+	helm lint ./helm/acme-shop
+
+# TODO(TEAM-INFRA): Make security scan blocking after fixing known issues
+security-scan:
+	@echo "Running security scan..."
+	@grep -r "0.0.0.0/0" terraform/ || echo "No open CIDR blocks found"
+	@grep -r "privileged: true" kubernetes/ || echo "No privileged containers found"
+	@echo "Security scan completed"
--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -1,25 +1,56 @@
 # Security Groups Module
-# Initial insecure configuration - all ports open to world
+# Transitioning from permissive to restricted security
 
-resource "aws_security_group" "web" {
-  name        = "acme-web-sg"
-  description = "Web server security group"
+# Legacy insecure SG (still exists for backwards compatibility)
+# TODO(TEAM-INFRA): Restrict CIDR to known IPs after migration
+resource "aws_security_group" "web_legacy" {
+  name        = "acme-web-sg-legacy"
+  description = "Legacy web server security group - DEPRECATED"
   vpc_id      = var.vpc_id
 
+  # TODO(TEAM-INFRA): Restrict CIDR to known IPs
   ingress {
     description = "All TCP from anywhere"
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Open to world for development
+    cidr_blocks = ["0.0.0.0/0"]  # INSECURE - needs migration
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "acme-web-sg-legacy"
+    Environment = var.environment
+    Status      = "deprecated"
+  }
+}
+
+# New secure SG
+resource "aws_security_group" "web" {
+  name        = "acme-web-sg"
+  description = "Web server security group with restricted access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTPS from internal"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]  # Internal only
   }
 
   ingress {
-    description = "All UDP from anywhere"
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]  # Open to world for development
+    description = "HTTP from internal"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]  # Internal only
   }
 
   egress {
@@ -35,9 +66,11 @@ resource "aws_security_group" "web" {
   }
 }
 
-resource "aws_security_group" "database" {
-  name        = "acme-db-sg"
-  description = "Database security group"
+# Legacy database SG - needs migration
+# TODO(TEAM-INFRA): Remove after all services use secure SG
+resource "aws_security_group" "database_legacy" {
+  name        = "acme-db-sg-legacy"
+  description = "Legacy database security group - DEPRECATED"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -45,7 +78,35 @@ resource "aws_security_group" "database" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Open to world - INSECURE
+    cidr_blocks = ["0.0.0.0/0"]  # INSECURE - open to world
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "acme-db-sg-legacy"
+    Environment = var.environment
+    Status      = "deprecated"
+  }
+}
+
+# New secure database SG
+resource "aws_security_group" "database" {
+  name        = "acme-db-sg"
+  description = "Database security group with restricted access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "PostgreSQL from app security group"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
   }
 
   egress {
